e901acfb21f2c2922866c66157a5e14cb6eb0ad0
Barbara Wahl <wahl@hanover.edu>
2013-05-30 11:26:48 -0400

    fixed spacing to make jslint happy

--- random.js
+++ random.js
@@ -1,28 +1,31 @@
 // local variable to hold Math.random
 var random = Math.random,
     floor = Math.floor,
-    maxInt = Math.pow(2,53);
+    maxInt = Math.pow(2, 53);
 
 // float function will return a random float in a given range 
 // min to max
 // if just one argument is provided, uses range 0 to arg
 // defaults to range 0..1
 function float(min, max) {
-	if (min === undefined) {
-		return random();
-	} else if (max === undefined) {
-		return random()*min;
-	} 		
-	return random()*(max - min) + min;	
+    if (min === undefined) {
+        return random();
+    } else if (max === undefined) {
+        return random() * min;
+    }
+    return random() * (max - min) + min;
 }
 
 // another version using the list of arguments
 function float2(min, max) {
-		switch (arguments.length) {
-		case 0: return random();
-		case 1: return random()*min;
-		default: return random()*(max - min) + min;
-	}
+    switch (arguments.length) {
+    case 0:
+        return random();
+    case 1:
+        return random() * min;
+    default:
+        return random() * (max - min) + min;
+    }
 }
 
 // returns a random integer
@@ -45,20 +48,20 @@ function int(min, max) {
 
 // create the return module 
 module.exports = {
-	float: float,
-	float2: float2,
-	int: int
-}
+    float: float,
+    float2: float2,
+    int: int
+};
 
 // tests that will not run from "require"
 if (require.main === module) {
-	console.log("float(5,10) = " , float(5,10));
-	console.log("float(10) = " , float(10));
-	console.log("float() = ", float());
-	console.log("float2(5,10) = " , float2(5,10));
-	console.log("float2(10) = " , float2(10));
-	console.log("float2() = ", float2());
-    console.log("int(5,10) = " , int(5,10));
-    console.log("int(10) = " , int(10));
+    console.log("float(5,10) = ", float(5, 10));
+    console.log("float(10) = ", float(10));
+    console.log("float() = ", float());
+    console.log("float2(5,10) = ", float2(5, 10));
+    console.log("float2(10) = ", float2(10));
+    console.log("float2() = ", float2());
+    console.log("int(5,10) = ", int(5, 10));
+    console.log("int(10) = ", int(10));
     console.log("int() = ", int());
 }
